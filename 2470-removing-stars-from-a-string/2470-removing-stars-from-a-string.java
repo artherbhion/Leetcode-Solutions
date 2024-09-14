e32651b633adfb11b318f206d80fc127272c44d7
artherbhion <147584933+artherbhion@users.noreply.github.com>
2024-09-14 10:40:29 +0530

    Time: 71 ms (60.82%), Space: 45.7 MB (71.45%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.java
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.java
@@ -9,12 +9,16 @@ class Solution {
                 stack.push(s.charAt(i));
             }
         }
-        StringBuilder s2=new StringBuilder();
-        while(!stack.isEmpty()){
-            s2.append(stack.pop());
-        }
-        s2.reverse();
-        String r=s2.toString();
-        return r;
+        // StringBuilder s2=new StringBuilder();
+        // while(!stack.isEmpty()){
+        //     s2.append(stack.pop());
+        // }
+        // s2.reverse();
+        // String r=s2.toString();
+        // return r;
+        int k=stack.size()-1;
+        char[] res = new char[stack.size()];
+         while(stack.size() > 0) res[k--] = stack.pop();
+        return new String(res);
     }
 }
