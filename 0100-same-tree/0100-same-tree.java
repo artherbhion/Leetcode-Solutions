7180400e868be3ba0a7bf40f3735806786ad0967
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2024-09-16 19:51:31 +0530

    Time: 0 ms (100%), Space: 41.3 MB (12.21%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null) return true;
+        else if(p==null || q==null){
+            return false;
+        }
+        else if(p.val==q.val){
+           return isSameTree(p.left, q.left)&& isSameTree(p.right, q.right);
+        }
+        return false;
+    }
+}
