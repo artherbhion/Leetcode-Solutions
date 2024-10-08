12c5bf06e3494f55bc699919bc2df6f5dfe0a481
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2024-10-08 21:50:46 +0530

    Time: 4 ms (70.44%), Space: 55.7 MB (86.46%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -5,7 +5,6 @@ class Solution {
             sum += nums[i];
         }
         double max = (double) sum / k;
-        System.out.print(max+" "+sum);
         for (int i = k; i < nums.length; i++) {
             sum += nums[i] - nums[i - k];
             max = Math.max(max, (double) sum / k);
