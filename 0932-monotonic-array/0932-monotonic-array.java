668a50c940f379c0af7d5fa9cbea21f10102dd6c
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2024-10-12 22:59:49 +0530

    Time: 6 ms (6.26%), Space: 59.5 MB (17.8%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        int ic=0,dc=0;
+     for(int i=0;i<nums.length-1;i++){
+        if(nums[i]<=nums[i+1]){
+            ic++;
+        }
+        if(nums[i]>=nums[i+1]){
+            dc++;
+        }
+     }
+     System.out.print(ic);
+     if(ic==nums.length-1||dc==nums.length-1){
+        return true;
+     }  
+     return false; 
+    }
+}
