1f265d01598420ba4cc4db435f9784f1918c5a69
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2024-11-02 11:46:40 +0530

    Time: 1 ms (96.81%), Space: 41.9 MB (72.21%) - LeetHub

---
+++ 2580-circular-sentence/2580-circular-sentence.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+         String str = sentence.trim();
+         int c=0;
+         for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==' '){
+                c++;
+                if(str.charAt(i-1)!=str.charAt(i+1)){
+                    return false;
+                }
+            }
+         }
+         if(c==0 && str.charAt(0)!=str.charAt(str.length()-1)){
+            return false;
+         }
+         if(str.charAt(0)!=str.charAt(str.length()-1)){
+            return false;
+         }
+         return true;
+    }
+}
