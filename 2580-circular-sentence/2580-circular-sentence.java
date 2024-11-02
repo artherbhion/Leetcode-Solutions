3a8142a7e794b9fba0ac932f8fb4bbc17afc88a4
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2024-11-02 11:48:40 +0530

    Time: 1 ms (96.81%), Space: 41.7 MB (90.5%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.java
+++ 2580-circular-sentence/2580-circular-sentence.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isCircularSentence(String str) {
+        if(str.charAt(0)!=str.charAt(str.length()-1)){
+            return false;
+         }
          int c=0;
          for(int i=0;i<str.length();i++){
             if(str.charAt(i)==' '){
@@ -11,10 +14,7 @@ class Solution {
          }
          if(c==0 && str.charAt(0)!=str.charAt(str.length()-1)){
             return false;
-         }
-         if(str.charAt(0)!=str.charAt(str.length()-1)){
-            return false;
-         }
+         }         
          return true;
     }
 }
