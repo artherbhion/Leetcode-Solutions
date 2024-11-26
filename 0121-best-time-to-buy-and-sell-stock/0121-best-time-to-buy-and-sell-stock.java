c13ce5cc1ebd8c0bce78ae8b02753e258a3557ff
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2024-11-26 08:37:43 +0530

    Time: 2 ms (77.25%), Space: 61.5 MB (72.59%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,11 +1,11 @@
 class Solution {
-    public int maxProfit(int[] nums) {
-        int mp=0,cp=0,min=10000;
-        for(int i=0;i<nums.length;i++){
-            min=Math.min(min,nums[i]);
-            cp=nums[i]-min;
-            mp=Math.max(cp,mp);
-        }
-        return mp;
+    public int maxProfit(int[] arr) {
+         int maxPro = 0;
+    int minPrice = Integer.MAX_VALUE;
+    for (int i = 0; i < arr.length; i++) {
+        minPrice = Math.min(minPrice, arr[i]);
+        maxPro = Math.max(maxPro, arr[i] - minPrice);
+    }
+    return maxPro;
     }
 }
