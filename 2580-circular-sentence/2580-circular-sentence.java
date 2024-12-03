db3708dfed7369ebec40c605dda63a6b751a2f89
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2024-12-03 18:49:13 +0530

    Time: 1 ms (88.57%), Space: 42.1 MB (21.84%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.java
+++ 2580-circular-sentence/2580-circular-sentence.java
@@ -3,8 +3,7 @@ class Solution {
        if(sentence.charAt(0)!=sentence.charAt(sentence.length()-1)) return false;
        String arr[]=sentence.split(" ");
        for(int i=0;i<arr.length-1;i++){
-        String s1=arr[i],s2=arr[i+1];
-        if(s1.charAt(s1.length()-1)!=s2.charAt(0)) return false;
+        if(arr[i].charAt(arr[i].length()-1)!=arr[i+1].charAt(0)) return false;
        }
        return true; 
     }
