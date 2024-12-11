a1072efcac293976b864ccb9ceb9b25d6d97e088
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2024-12-11 10:18:01 +0530

    Time: 1 ms (98.36%), Space: 43.1 MB (61.7%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -1,11 +1,14 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
+        HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<arr.length;i++){
-           for(int j=0;j<arr.length;j++){
-                if(arr[i]==(arr[j]*2) && i!=j){
-                    return true;
-                }
-    }
-        }return false;
+            if(map.containsKey(arr[i]*2)|| (arr[i]%2==0 && map.containsKey(arr[i]/2))){
+                return true;
+            }
+            else{
+                map.put(arr[i],i);
+            }
+        }
+        return false;
 }
 }
