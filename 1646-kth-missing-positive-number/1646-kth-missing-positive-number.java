abe19d93398d506262cb75a100646d8c97220652
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2024-12-19 21:00:14 +0530

    Time: 4 ms (8.05%), Space: 44.3 MB (5.65%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
@@ -1,14 +1,14 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        HashMap<Integer,Integer> map=new HashMap<>();
+       HashSet<Integer> set=new HashSet<>();
         int max=0;
         for(int i=0;i<arr.length;i++){
-            map.put(i,arr[i]);
+            set.add(arr[i]);
             max=Math.max(arr[i],max);
         }
         int c=0;
         for(int i=1;i<max;i++){
-            if(!map.containsValue(i)){
+            if(!set.contains(i)){
                 c++;
             }
             if(c==k){
