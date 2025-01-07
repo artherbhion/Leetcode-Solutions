4a26a8bddb4f1930bb31f268d079635ac63ffe91
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2025-01-07 17:48:17 +0530

    Time: 1 ms (100%), Space: 57.5 MB (36.51%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,31 +1,30 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        Arrays.sort(nums);
-        int l=nums.length;
-        if(nums[0]>1)return 1;
-        for(int i=0;i<l;i++){
-            if(nums[i]>0 ){
-                if(i+1<l){
-                if((nums[i+1]) != (nums[i]+1) ){
-                    int c=nums[i];
-                    return(c+1);
-                }
-                else if(i-1 >0){
-                 if(nums[i-1]!=nums[i]-1){
-                     int c=nums[i];
-                    return(c-1);
-                }}
-                }
-                else{
-                    return nums[i]+1;
-                    }
+        // data filtering
+        for(int i = 0; i<nums.length;i++){
+            if(nums[i]<=0 || nums[i]>= nums.length+1){
+                nums[i] = nums.length+1;
             }
         }
-        if(nums[0]==0){
-            return 1;
-        }if(nums[0]<0){
-             return 1;
+
+        // data visited and then mark that index as -ve
+        for(int i = 0;i<nums.length;i++){
+            int element = Math.abs(nums[i]);
+            if(element == nums.length+1){
+                continue;
+            }
+            int seat = element -1;
+            if(nums[seat] > 0){
+                nums[seat] = -nums[seat];
+            }
         }
-        return 0;
+
+        // 
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i]>0){
+                return i+1;
+            }
+        }
+        return nums.length+1;
     }
 }
