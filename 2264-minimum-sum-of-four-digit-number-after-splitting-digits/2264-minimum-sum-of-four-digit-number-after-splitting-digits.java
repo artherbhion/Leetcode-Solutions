310c36441530a6db8fe65db2b9a8392e543e0d3d
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2025-02-06 23:31:31 +0530

    Time: 13 ms (5.75%), Space: 41.4 MB (5.94%) - LeetHub

---
+++ 2264-minimum-sum-of-four-digit-number-after-splitting-digits/2264-minimum-sum-of-four-digit-number-after-splitting-digits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumSum(int num) {
+        int a= (num/1000*10)+(num/100)%10;
+        int b=(num%100);
+        int c=(num/1000*10)+(num%10);
+        int d=(num%1000)/10;
+        int e = ((num/100)%10)*10+(num/1000);  
+        int f=(num%10)*10+(num%100)/10;
+        int g=num%10*10+num/1000;
+        int h=num/1000*10+(num%100)/10;
+        int i=((num/100)%10)*10+num%10;
+        int j=num%10*10+(num%1000)/100;
+        int k=(num%100)/10*10+num/1000;
+        System.out.println(a+" "+b);
+        System.out.println(c+" "+d);
+        System.out.println(e+" "+k+" "+j);
+        return Math.min(a+b,Math.min(j+k,Math.min(b+e,Math.min(c+d,Math.min(d+g,Math.min(a+f,h+i))))));
+    }
+}
