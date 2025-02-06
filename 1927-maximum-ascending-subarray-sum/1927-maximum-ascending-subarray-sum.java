f84f8b7babb297074bdd57806ff2ce2bdfd8fa43
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2025-02-06 22:20:19 +0530

    Time: 0 ms (100%), Space: 41 MB (88.86%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
@@ -3,16 +3,9 @@ class Solution {
         int max=0,curr=nums[0];
         for(int i=1;i<nums.length;i++){
             max=Math.max(curr,max);
-            if(nums[i]>nums[i-1]){
-                curr+=nums[i];
-            }
-            else{ 
-                // max=Math.max(curr,max);             
-               curr=nums[i];
-            } 
-            System.out.println(curr+" "+max);
-        } 
-        
+            if(nums[i]>nums[i-1])curr+=nums[i];
+            else curr=nums[i];
+        }         
         return curr>max?curr:max;
     }
 }
