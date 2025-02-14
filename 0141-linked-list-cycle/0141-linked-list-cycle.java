6364b7b314536ae246a80ff20958844ef7f7819d
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2025-02-14 08:23:35 +0530

    Time: 0 ms (100%), Space: 44.4 MB (75.7%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,9 +11,6 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-       if(head==null){
-        return false;
-       }
        ListNode s=head;
        ListNode f=head;
        while(f!=null &&f.next!=null){
