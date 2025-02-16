a8c56f8ab5000cdd9a8441692bd162788016a869
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2025-02-16 10:42:13 +0530

    Time: 4 ms (99.92%), Space: 46.4 MB (5.47%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -39,10 +39,10 @@ class Solution {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == 1) {
-                    if (i == 0 || grid[i - 1][j] == 0) perimeter++; // Top
-                    if (i == rows - 1 || grid[i + 1][j] == 0) perimeter++; // Bottom
-                    if (j == 0 || grid[i][j - 1] == 0) perimeter++; // Left
-                    if (j == cols - 1 || grid[i][j + 1] == 0) perimeter++; // Right
+                    if (i == 0 || grid[i - 1][j] == 0) perimeter++;
+                    if (i == rows - 1 || grid[i + 1][j] == 0) perimeter++;
+                    if (j == 0 || grid[i][j - 1] == 0) perimeter++;
+                    if (j == cols - 1 || grid[i][j + 1] == 0) perimeter++;
                 }
             }
         }
