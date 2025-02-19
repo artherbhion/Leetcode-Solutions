970500605214503d9f9873a4a0f25a7ec455536f
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2025-02-19 09:17:28 +0530

    Time: 4 ms (75.54%), Space: 43.4 MB (69.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
-        HashMap<Character, Integer> map = new HashMap<>();
+        int[] arr=new int[26];
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            map.put(c, map.getOrDefault(c, 0) + 1);
+            arr[c-'a']++;
         }
         for (int i = 0; i < t.length(); i++) {
             char c = t.charAt(i);
-            if (!map.containsKey(c) || map.get(c) == 0) {
-                return false;
-            }
-            map.put(c, map.get(c) - 1);
+            arr[c-'a']--;
         }
-        for (char key : map.keySet()) {
-            if (map.get(key) != 0) return false;
+        for (int i : arr) {
+            if (i != 0) return false;
         }
         return true;
     }
