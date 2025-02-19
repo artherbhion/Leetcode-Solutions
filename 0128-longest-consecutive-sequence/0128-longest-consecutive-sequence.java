617067635c344015df8bca375a422b55f3aed441
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2025-02-19 11:56:04 +0530

    Time: 17 ms (91.08%), Space: 56.8 MB (82.52%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,21 +1,29 @@
+import java.util.Arrays;
+
 class Solution {
     public int longestConsecutive(int[] nums) {
-       HashSet<Integer> map=new HashSet<>();
-       int n=nums.length;
-       for(int i=0;i<n;i++){
-        map.add(nums[i]);
-       }
-       int max=0;
-       for (int num : map){        
-         if (!map.contains(num - 1)) {
-                int cs = 1;
-                while(map.contains(num+1)){
-                        cs++;
-                        num=num+1;
+        int n = nums.length;
+        
+        if (n == 0) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+
+        int cnt = 1;
+        int maxi = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] != nums[i - 1]) {
+                if (nums[i] == nums[i - 1] + 1) {
+                    cnt++;
+                } else {
+                    maxi = Math.max(maxi, cnt);
+                    cnt = 1;
                 }
-        max=Math.max(cs,max);
-         }
-       }
-       return max;
+            }
+        }
+
+        return Math.max(maxi, cnt);
     }
 }
