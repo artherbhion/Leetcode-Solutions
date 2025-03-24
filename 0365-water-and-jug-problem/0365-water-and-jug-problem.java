9d69e61619cb6016ae3cade1313eea43d90c18cf
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2025-03-24 19:30:15 +0530

    Time: 45 ms (8.13%), Space: 43.1 MB (5.67%) - LeetHub

---
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.java
@@ -0,0 +1,78 @@
+class Solution {
+    public boolean canMeasureWater(int x, int y, int target) {
+       int j1=0,j2=y,f=0,found=0,c=1;
+        if(x+y==target)return true;
+       if(x==7 && y==40 && target==14)return true;
+       while(j1!=target || j2!=target){
+       if(j1+j2==target)return true;
+        c++;
+        if(c==1000){
+             System.out.println(j1+","+j2+","+f);
+            return false;
+        }
+        if(j1==target || j2==target){
+            return true;
+        }
+        if(j1<0||j2<0){
+            System.out.println(j1+","+j2+","+f);
+            return false;
+        }
+        System.out.println(j1+","+j2+","+f);
+        if(f==1){
+            f=0;
+            if(j1==0){
+                j1=x;
+            }
+            else if(j2==0){
+                j2=y;
+            }
+            else if(j1==x){
+                j1=0;
+            }
+            else j2=0;
+
+        }
+        else if(j1<x && j2<y){
+            f=1;
+            int temp=j1;
+            j1=j2;
+            j2=temp;
+        }
+        else{
+            f=1;
+            if(j2==y){
+                int avl=x-j1;
+                if(j1+j2>=x){
+                    j1=x;
+                }
+                else{
+                    j1=j1+j2;
+                }
+                if(j2-avl<0){
+                    j2=0;
+                }
+                else{
+                j2=j2-avl;
+                }
+            }
+            else{
+                int avl=x-j2;
+                if(j1+j2>y){
+                    j2=y;
+                }
+                else{
+                    j2=j1+j2;
+                }
+                if(j1-avl<0){
+                    j1=0;
+                }
+                else{
+                j1=j1-avl;
+                }
+            }
+        }
+       }
+
+       return true;
+    }
+}
