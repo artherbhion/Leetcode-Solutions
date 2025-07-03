b8e6ca9b67ef241ae47b5b9bfdd9d3711eae96d6
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2025-07-03 18:21:44 +0530

    Time: 179 ms (3.47%), Space: 45.2 MB (98.36%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,31 +1,43 @@
-public class Solution {
+class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int count = 0;
-        for(int i = 0; i < flowerbed.length; i++) {
-            int next=2,pre=2;
-            if(flowerbed[i] == 0) {
-                if(i==flowerbed.length-1){
-                     next=0;
-                }
-                else{
-                     next=flowerbed[i + 1];
-                }
-                if(i==0){
-                     pre=0;
-                }
-                else{
-                     pre=flowerbed[i-1];
-                }
-               if(next == 0 && pre == 0) {
-                   flowerbed[i] = 1;
-                   count++;
-               }
+        if(n==0)return true;
+        if(flowerbed.length==1 && flowerbed[0]==0){
+            n--;
+            flowerbed[0]=1;
+        }
+        if(flowerbed.length>=2){
+        if(flowerbed[0]==0 && flowerbed[1]==0){
+            n--;
+            flowerbed[0]=1;
+
+        }
+        if(flowerbed[flowerbed.length-2]==0 && flowerbed[flowerbed.length-1]==0){
+            n--;
+            flowerbed[flowerbed.length-1]=1;
+        }
+        }
+        for(int i=1;i<flowerbed.length-1;i++){
+            if(flowerbed[i]==0){
+                 if(flowerbed[i-1]==0 && flowerbed[i+1]==0){
+                    n--;
+                    flowerbed[i]=1;
+                 }
             }
         }
-        if(count>=n){
+         for(int i=0;i<flowerbed.length;i++){
+        System.out.print(flowerbed[i]);
+         }
+         if(flowerbed.length>2){
+        if(flowerbed[0]==0 && flowerbed[1]==0){
+            n--;
+        }
+        if(flowerbed[flowerbed.length-2]==0 && flowerbed[flowerbed.length-1]==0){
+            n--;
+        }
+        }
+        if(n<=0){
             return true;
         }
-        
         return false;
     }
 }
