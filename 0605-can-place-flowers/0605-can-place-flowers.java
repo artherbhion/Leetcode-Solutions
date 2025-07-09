ffabc2f2c87b3baf6f501c2bba8d2cac91097aa1
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2025-07-09 13:22:57 +0530

    Time: 87 ms (3.25%), Space: 45.3 MB (97.21%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -3,11 +3,11 @@ class Solution {
         int len = flowerbed.length;
         int i = 0;
         while(i < len && n > 0) {
+                System.out.print(i+" ");
             if(flowerbed[i] == 1) i += 2;
             else if(i==flowerbed.length-1||flowerbed[i + 1] == 0) {
                 n--;
-                i += 2;
-                System.out.print(i);
+                i+= 2;
             }
             else i += 3;
         }
