9a6fbd980682a3a9ae694e108eddeab41c222bfa
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2025-07-12 18:59:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -1,16 +1,7 @@
 class Solution {
     public int tribonacci(int n) {
-        if(n==0)
-            return 0;
-        int[]dp={0,1,1};
-        for(int i=3;i<=n;i++)
-        {
-            int next=dp[0]+dp[1]+dp[2];
-            dp[0]=dp[1];
-            dp[1]=dp[2];
-            dp[2]=next;
-        }
-        return dp[2];
-        
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+        return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
     }
 }
