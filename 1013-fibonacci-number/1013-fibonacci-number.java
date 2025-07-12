efd4a781fa3c10975a0b5928e171e99197d7ec88
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2025-07-12 18:54:19 +0530

    Time: 10 ms (10.17%), Space: 40.2 MB (80.42%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,14 +1,7 @@
 class Solution {
     public int fib(int n) {
-        if(n==0){
-            return 0;
-        }
-       int pre=0,cur=1;
-       for(int i=2;i<=n;i++){
-        int temp=cur;
-        cur=cur+pre;
-        pre=temp;
-       }
-       return cur;
+      if(n==0)return 0;
+      if(n==1)return 1;
+      return fib(n-2)+fib(n-1);
     }
 }
