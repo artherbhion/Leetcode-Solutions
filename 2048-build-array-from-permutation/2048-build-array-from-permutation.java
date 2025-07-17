59a4080f5da36b27bd26b76fd1b44d1e6196eba8
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2025-07-17 18:29:20 +0530

    Time: 1 ms (99.1%), Space: 45.2 MB (92.85%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.java
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-        int[] ans=nums.clone();
+        int[] ans=new int[nums.length];
         for(int i=0;i<nums.length;i++){
             ans[i]=nums[nums[i]];
         }
