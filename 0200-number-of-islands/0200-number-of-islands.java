2b9e8010ddb3f1143542f0561848ec2c9a2078de
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2025-07-17 16:23:28 +0530

    Time: 2 ms (99.82%), Space: 49.2 MB (89.07%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,32 +1,28 @@
 class Solution {
+    static int r;
+    static int c;
     public int numIslands(char[][] grid) {
-        int r=grid.length;
-        int c=grid[0].length;
+        r=grid.length;
+        c=grid[0].length;
         int count=0;
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(grid[i][j]=='1'){
                     count++;
-                    makezero(i,j,grid,r,c);
+                    makezero(i,j,grid);
                 }
             }
         }
         return count;
     }
-    public static void makezero(int i,int j,char[][] grid,int r,int c){
-        if(i<0 || j<0){
-            return;
-        }
-        if(i>=r || j>=c){
-            return;
-        }
-        if(grid[i][j]=='0'){
+    public static void makezero(int i,int j,char[][] grid){
+        if(i<0 || j<0 ||i>=r || j>=c || grid[i][j]=='0'){
             return;
         }
         grid[i][j]='0';
-        makezero(i-1,j,grid,r,c);
-        makezero(i+1,j,grid,r,c);
-        makezero(i,j+1,grid,r,c);
-        makezero(i,j-1,grid,r,c);
+        makezero(i-1,j,grid);
+        makezero(i+1,j,grid);
+        makezero(i,j-1,grid);
+        makezero(i,j+1,grid);
     }
 }
