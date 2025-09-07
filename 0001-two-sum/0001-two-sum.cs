bf4021eb4062333ce554c68716aad7ea0be87969
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2025-09-07 11:03:03 +0530

    Time: 31 ms (25.28%), Space: 48.4 MB (83.64%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+       int n=nums.Length;
+        for (int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[] { i, j };
+
+                }
+            }
+        }
+        return new int[0]; // empty array
+
+    }
+}
